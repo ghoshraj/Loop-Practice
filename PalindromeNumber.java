b284a3bcfa3c37bffb585d6744ab2292afb1e59a
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-07-31 12:05:41 +0530

    First Commit

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -2,17 +2,15 @@ package loop.com;
 
 public class PalindromeNumber {
     public static void main(String[] args) {
-        int num = 99;
+        int num = 111;
         int temp = num;
         int sum = 0;
-        int i = 1;
         while (temp > 0 ){
             int r = temp % 10;
-            i = (sum * 10 ) + r;
-            sum = sum + r;
+            sum = (sum * 10 ) + r;
             temp = temp/ 10;
         }
-        if (num == i){
+        if (num == sum){
             System.out.println("this is a palindrome number");
         }
         else {
