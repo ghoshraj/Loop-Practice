a479f0972d357cbe12565a9ef9f6eeae33c1e959
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-07-31 11:18:55 +0530

    First Commit

--- AutomorphicNumber.java
+++ AutomorphicNumber.java
@@ -8,7 +8,17 @@ public class AutomorphicNumber {
         System.out.println("enter the number : ");
         int n = sc.nextByte();
         int b = n * n;
-        if (n == b % 10){
+        int count = 0;
+        int c = n;
+        int d = 1;
+        while (c > 0){
+            count++;
+            c = c/10;
+        }
+        for (int i = 1; i <= count; i++){
+            d = d * 10;
+        }
+        if (n == b % d){
             System.out.println("This is a automorphic number .....");
         }
         else {
